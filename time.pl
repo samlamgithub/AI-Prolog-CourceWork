56f2a005863ff199b6b1304f78fcf2515f5dae84
Jiahao Lin <jl6613@ray38.doc.ic.ac.uk>
2015-02-11 11:05:37 +0000

    changed time.pl

--- time.pl
+++ time.pl
@@ -1,20 +1,22 @@
 
-
-
-
-time_stamp(X):- get_time(X).
+time_stamp(X):- statistics(runtime, [Z| _]), X is Z*1000.
 
 
 time_diff(Diff):-
-time_stamp(X),
-do(100),
-time_stamp(Y),
-Diff is Y-X.
+statistics(runtime, [X,_]),
+write(X),
+nl,
+do(10000),
+statistics(runtime, [Y,_]),
+write(Y),
+Diff is Y-X,
+write('Now is diff'),
+nl,
+format(' took ~20d sec.~n', [Diff]).
 
 do(0):- V is 56874632+67532436475.
 
 do(X):- 
 X \= 0,
-V is 56874632+67532436475,
 Y is X-1,
 do(Y). 
